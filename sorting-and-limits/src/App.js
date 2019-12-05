7260364a37766e5b21b21c39dc277f310237deed
edwardmartinsjr <edwardmartinsjr@gmail.com>
2019-12-05 09:55:21 +0100

    add sorting-and-limits project

---
+++ sorting-and-limits/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { jexiaClient, dataOperations } from "jexia-sdk-js/node";
+import request from 'superagent'
+// const superagent = require('superagent');
+
+const dataModule = dataOperations();
+
+async function App() {
+
+  // You need to use API Key / API Secret which is generated in Jexia. Do not forget make Policy for API   
+  jexiaClient().init({
+    projectID: "project_id",
+    key: "API_KEY",
+    secret: "API_SECRET",
+  },dataModule);
+
+  //sorting 
+  const sortedPosts = await dataModule.dataset("posts")
+  .select()
+  .sortAsc("created_at")  // .sortDesc("created_at")
+  .execute();
+
+  console.log(sortedPosts)
+
+  // Limits and Offsets
+  return await dataModule.dataset("posts")
+  .select()
+  .limit(2)
+  .offset(5)
+  .execute();
+}
+
+export default App;
